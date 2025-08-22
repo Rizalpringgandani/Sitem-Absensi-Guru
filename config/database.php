4d6e478a60e490a41ac725926cf070e7990b8bea
Rizalpringgadani <pringgandanirizal37@gmail.com>
2025-08-22 09:28:51 +0700

    integrasi composer dan file .env untuk keamanan

--- config/database.php
+++ config/database.php
@@ -1,12 +1,19 @@
 <?php
-$host = "localhost";
-$user = "root"; // sesuaikan dengan user Anda
-$pass = ""; // sesuaikan dengan password Anda
-$db   = "db_absenguru"; // sesuaikan dengan nama database Anda
+// Muat autoloader Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Gunakan Dotenv untuk memuat variabel dari file .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Ambil variabel dari environment
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$db   = $_ENV['DB_NAME'];
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
